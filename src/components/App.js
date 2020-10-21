4aa9eb4a63f4f5d18122373ddaedcb72af527568
Amit Raj <amitraj9046@gmail.com>
2020-10-21 21:25:54 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,30 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [output, setOutput] = React.useState(false);
+
+  const handleClick = () => {
+    setOutput(true);
+    console.log(output);
+  };
+  function Para() {
+    return (
+      <p id="para">
+        Hello, I've learnt to use the full-stack evaluation tool. This makes me
+        so happy
+      </p>
+    );
+  }
   return (
     <div id="main">
-      // Do not alter the main div
+      {/* // Do not alter the main div */}
+      <button id="click" onClick={handleClick}>
+        click
+      </button>
+      {output ? <Para /> : ""}
     </div>
   );
 }
-
 
 export default App;
